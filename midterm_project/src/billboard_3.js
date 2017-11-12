fb1e58979e6d7158f7245bde74f767444af013ab
Jegal Jihye Mac <jkjh0819@naver.com>
2017-11-12 18:07:08 +0900

    finish third example

--- midterm_project/src/billboard_3.js
+++ midterm_project/src/billboard_3.js
@@ -1,10 +1,38 @@
 var chart = bb.generate({
-    bindto: "#chart",
-    data: {
-        type: "bar",
-        columns: [
-            ["data1", 30, 200, 100, 170, 150, 250],
-            ["data2", 130, 100, 140, 35, 110, 50]
-        ]
+  "data": {
+    "type": "line",
+    "xs": {
+      "male": "x1",
+      "female": "x2"
+    },
+    "columns": [
+    	["x1", 0, 10, 20, 30, 40, 50, 60, 70, 80],
+    	["x2", 0, 10, 20, 30, 40, 50, 60],
+    	["male", 41, 88, 83, 79, 79, 87, 87, 100, 0],
+    	["female", 37, 24, 28, 17, 31, 11, 0]
+    ],
+    "labels": {
+      "position": {
+        "x": 0,
+        "y": 4
+      }
     }
+  },
+
+  "subchart": {
+    "show": true
+  },
+
+  "axis": {
+    "y": {
+      "min" : 10,
+      "max" : 90,
+      "tick": {
+        "format": function (x) {
+          return x + "%";
+        }
+      }
+    }
+  },
+  "bindto": "#chart"
 });
--- midterm_project/src/d3_3.js
+++ midterm_project/src/d3_3.js
@@ -1,50 +1,67 @@
-(function () {
-  var data = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+ var svg = d3.select("#chart").append("svg")
+      .attr("width", 960)
+      .attr("height", 300)
 
-  var w = 400,
-      h = 110;
+var margin = {left:40, right:30, top: 10, bottom: 20}
+var width = svg.attr("width") - margin.left - margin.right;
+var height = svg.attr("height") - margin.bottom - margin.top;
 
-  var svg = d3.select("#chart")
-      .append("svg")
-      .attr("width", w)
-      .attr("height", h);
+var data = [{"ages":"0", "female": 0.37, "male": 0.41},
+            {"ages":"10", "female": 0.24, "male": 0.88},
+            {"ages":"20", "female": 0.28, "male": 0.83},
+            {"ages":"30", "female": 0.17, "male": 0.79},
+            {"ages":"40", "female": 0.31, "male": 0.79},
+            {"ages":"50", "female": 0.11, "male": 0.87},
+            {"ages":"60", "female": 0, "male": 0.87},
+            {"ages":"70", "female": 0, "male": 1.00},
+            {"ages":"80", "female": 0, "male": 0},
+           ];
 
+var x = d3.scaleLinear()
+  .rangeRound([0,width]);
 
-  svg.selectAll("rect")
-      .data(data)
-      .enter()
-      .append("rect")
-      .attr("x", function(d, i) {
-        return i * (w / data.length)
-      })
-      .attr("y", function(d) {
-        return h - d;
-      })
-      .attr("width", w / data.length - 1)
-      .attr("height", function(d) {
-        return d;
-      })
-      .attr("fill", function(d) {
-        return "hotpink";
-      });
+var y = d3.scaleLinear()
+  .rangeRound([height, 0]);
 
-  svg.selectAll("text")
-      .data(data)
-      .enter()
-      .append("text")
-      .text(function(d) {
-        return d;
-      })
-      .attr("x", function(d, i) {
-        return i * (w / data.length) + (w / data.length) / 2;
-      })
-      .attr("y", function(d) {
-        return h - d + 10;
-      })
-      .attr("font-family", "sans-serif")
-      .attr("font-size", "11px")
-      .attr("fill", "black")
-      .attr("text-anchor", "middle");
+x.domain(d3.extent(data, function(d) { return d.ages; }));
+y.domain([0, 
+          d3.max(data, function(d) { 
+            return d3.max([d.female, d.male]);
+          })]);
 
+var multiline = function(category) {
+  var line = d3.line()
+              .x(function(d) { return x(d.ages); })
+              .y(function(d) { return y(d[category]); });
+  return line;
+}
 
-})();
+var line = d3.line()
+  .x(function(d) { return x(d.ages); })
+  .y(function(d) { return y(d); }); 
+
+var categories = ['female', 'male'];
+var color = d3.scaleOrdinal(d3.schemeCategory10);
+
+var g = svg.append("g")
+    .attr("transform",
+      "translate(" + margin.left + "," + margin.top + ")");
+
+for (i in categories) {
+  var lineFunction = multiline(categories[i]);
+  g.append("path")
+    .datum(data) 
+    .attr("class", "line")
+    .style("stroke", color(i))
+    .attr("d", lineFunction);
+}
+
+  // Add the X Axis
+  g.append("g")
+  .attr("transform", "translate(0," + height + ")")
+  .call(d3.axisBottom(x));
+
+  // Add the Y Axis
+  g.append("g")
+  .call(d3.axisLeft(y)
+          .tickFormat(d3.format(".0%")));
