c19626b84e6d1017007cd1f1e88ac7e492ec3c6d
Jegal Jihye Mac <jkjh0819@naver.com>
2017-11-13 16:36:15 +0900

    edit explanation

--- midterm_project/src/d3_1.js
+++ midterm_project/src/d3_1.js
@@ -3,7 +3,7 @@ var width = 960,
     radius = Math.min(width, height) / 2;
 
 var color = d3.scaleOrdinal()
-    .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
+    .range(["#98abc5", "#7b6888", "#d0743c"]);
 
 
 var percentageFormat = d3.format("%");
