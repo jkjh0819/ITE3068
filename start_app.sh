5ff46e5d11fe75aed3e1f2abc7704c293f81f08c
Jegal Jihye Mac <jkjh0819@naver.com>
2017-12-14 01:13:47 +0900

    hubblemon branch

--- start_app.sh
+++ start_app.sh
@@ -1,6 +1,7 @@
-docker run -d --name="arcus-admin" -h "arcus" ruo91/arcus
-docker run -d --name="arcus-memcached-1" -p 11211:11211 -h "memcached-1" ruo91/arcus:memcached
-docker run -d --name="arcus-memcached-2" -h "memcached-2" ruo91/arcus:memcached
-docker run -d --name="arcus-memcached-3" -h "memcached-3" ruo91/arcus:memcached
-docker run -d --name=mysql -p 3306:3306 --env MYSQL_ROOT_PASSWORD=root --env MYSQL_USER=team6 --env MYSQL_PASSWORD=team6 --env MYSQL_DATABASE=team6 camelia0858/mysql:latestest
-docker run -d --name=nbase-arc -p 6000:6000 hyeongseok05/nbase-arc
+docker run -d --net=ite3068 --name="arcus-admin" -h "arcus" ruo91/arcus
+docker run -d --net=ite3068 --name="arcus-memcached-1" -p 11211:11211 -p 4120:4120 -h "memcached-1" ruo91/arcus:memcached
+docker run -d --net=ite3068 --name="arcus-memcached-2" -h "memcached-2" ruo91/arcus:memcached
+docker run -d --net=ite3068 --name="arcus-memcached-3" -h "memcached-3" ruo91/arcus:memcached
+docker run -d --net=ite3068 --name=mysql -p 3306:3306 -p 4257:4257 --env MYSQL_ROOT_PASSWORD=root --env MYSQL_USER=team6 --env MYSQL_PASSWORD=team6 --env MYSQL_DATABASE=team6 camelia0858/mysql:latestest
+docker run -d --net=ite3068 --name=nbase-arc -p 6000:6000 hyeongseok05/nbase-arc
+#docker run -d --net=ite3068 --name="hubblemon" -h "hubblemon" -p 40000:40000 jkjh0819/ite3068:hubblemon
