eed75d071fdef05146c851ccbe2a307849d62c02
Jegal Jihye Mac <jkjh0819@naver.com>
2017-12-15 03:17:48 +0900

    remove unimportant annotation

--- start_app.sh
+++ start_app.sh
@@ -4,4 +4,3 @@ docker run -d --net=ite3068 --name="arcus-memcached-2" -h "memcached-2" ruo91/ar
 docker run -d --net=ite3068 --name="arcus-memcached-3" -h "memcached-3" ruo91/arcus:memcached
 docker run -d --net=ite3068 --name=mysql -p 3306:3306 -p 4257:4257 --env MYSQL_ROOT_PASSWORD=root --env MYSQL_USER=team6 --env MYSQL_PASSWORD=team6 --env MYSQL_DATABASE=team6 jkjh0819/ite3068:mysql-hubble
 docker run -d --net=ite3068 --name=nbase-arc -p 6000:6000 -p 4142:4142 jkjh0819/ite3068:nbase-hubble
-#docker run -d --net=ite3068 --name="hubblemon" -h "hubblemon" -p 40000:40000 jkjh0819/ite3068:hubblemon
