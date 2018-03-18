6243ed4dfcada6ffc5240e0db020ae738475b158
jihye-desktop <jkjh0819@naver.com>
2018-03-18 21:40:34 +0900

    change directory

--- start_app.sh
+++
@@ -1,6 +0,0 @@
-docker run -d --net=ite3068 --name="arcus-admin" -h "arcus" jkjh0819/ite3068:arcus-admin
-docker run -d --net=ite3068 --name="arcus-memcached-1" -p 11211:11211 -p 4120:4120 -h "memcached-1" jkjh0819/ite3068:arcus-hubble
-docker run -d --net=ite3068 --name="arcus-memcached-2" -h "memcached-2" ruo91/arcus:memcached
-docker run -d --net=ite3068 --name="arcus-memcached-3" -h "memcached-3" ruo91/arcus:memcached
-docker run -d --net=ite3068 --name=mysql -p 3306:3306 -p 4257:4257 --env MYSQL_ROOT_PASSWORD=root --env MYSQL_USER=team6 --env MYSQL_PASSWORD=team6 --env MYSQL_DATABASE=team6 jkjh0819/ite3068:mysql-hubble
-docker run -d --net=ite3068 --name=nbase-arc -p 6000:6000 -p 4142:4142 jkjh0819/ite3068:nbase-hubble
--- stop_app.sh
+++
@@ -1,6 +0,0 @@
-docker rm -f arcus-admin
-docker rm -f arcus-memcached-1
-docker rm -f arcus-memcached-2
-docker rm -f arcus-memcached-3
-docker rm -f mysql
-docker rm -f nbase-arc
